db4ac6bf5d9e69bf03e36ed63f54ed71b425b11e
Fernando Rafael Carretto Sosa <fernandocarretto@locus.software>
2022-04-02 13:25:05 -0300

    Site finalizado

--- src/App.js
+++ src/App.js
@@ -3,21 +3,32 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" align="center" id="container">
+      <h1>Política de Privacidade - CryptoApp</h1>
+      <p align="justify">A sua privacidade é importante para nós. É política do CryptoApp respeitar a sua privacidade em relação a qualquer informação sua que possamos coletar no site CryptoApp, e outros sites que possuímos e operamos.
+
+Solicitamos informações pessoais apenas quando realmente precisamos delas para lhe fornecer um serviço. Fazemo-lo por meios justos e legais, com o seu conhecimento e consentimento. Também informamos por que estamos coletando e como será usado.
+
+Apenas retemos as informações coletadas pelo tempo necessário para fornecer o serviço solicitado. Quando armazenamos dados, protegemos dentro de meios comercialmente aceitáveis ​​para evitar perdas e roubos, bem como acesso, divulgação, cópia, uso ou modificação não autorizados.
+
+Não compartilhamos informações de identificação pessoal publicamente ou com terceiros, exceto quando exigido por lei.
+
+O nosso site pode ter links para sites externos que não são operados por nós. Esteja ciente de que não temos controle sobre o conteúdo e práticas desses sites e não podemos aceitar responsabilidade por suas respectivas políticas de privacidade.
+
+Você é livre para recusar a nossa solicitação de informações pessoais, entendendo que talvez não possamos fornecer alguns dos serviços desejados.
+
+O uso continuado de nosso site será considerado como aceitação de nossas práticas em torno de privacidade e informações pessoais. Se você tiver alguma dúvida sobre como lidamos com dados do usuário e informações pessoais, entre em contacto connosco.</p>
+<h2>Compromisso do Usuário</h2>
+<p align="justify">O usuário se compromete a fazer uso adequado dos conteúdos e da informação que o CryptoApp oferece no site e com caráter enunciativo, mas não limitativo: </p>
+<ul align="justify">
+<li>Não se envolver em atividades que sejam ilegais ou contrárias à boa fé a à ordem pública;</li>
+<li>Não difundir propaganda ou conteúdo de natureza racista, xenofóbica, Onde ver a Bola ou azar, qualquer tipo de pornografia ilegal, de apologia ao terrorismo ou contra os direitos humanos;</li>
+<li>Não causar danos aos sistemas físicos (hardwares) e lógicos (softwares) do CryptoApp, de seus fornecedores ou terceiros, para introduzir ou disseminar vírus informáticos ou quaisquer outros sistemas de hardware ou software que sejam capazes de causar danos anteriormente mencionados.</li>
+</ul>
+<h2>Compromisso do Usuário</h2>
+<p align="justify">Esperemos que esteja esclarecido e, como mencionado anteriormente, se houver algo que você não tem certeza se precisa ou não, geralmente é mais seguro deixar os cookies ativados, caso interaja com um dos recursos que você usa em nosso site.
+
+Esta política é efetiva a partir de Abril/2022.</p>
     </div>
   );
 }
